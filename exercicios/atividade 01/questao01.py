ec9987993fbec3ac871d090b92ee9181925279e9
thiagomeloo <4thiagomelo5@gmail.com>
2021-01-14 13:23:17 -0300

    resolucao atividade01-questao01

---
+++ exercicios/atividade 01/questao01.py
@@ -0,0 +1,10 @@
+# construa uma função que receba dois vetores 
+# e retorne um vetor com os
+# valores que estão presentes nos dois vetores passados.
+
+vetor1 = [1,2,3]
+vetor2 = [4,5,6]
+
+soma = vetor1 + vetor2
+
+print(soma)
